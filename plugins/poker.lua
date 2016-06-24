0d69ce48553f0a652c03d40084f527e40440198b
Seyed Ali Kia <seyedkia27@gmail.com>
2016-06-24 16:32:48 +0430

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -2,8 +2,8 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = '😐'
-if matches[1] == '😐' then
+local text = '😐,:|,|:,:/,/:,:\,\:'
+if matches[1] == '😐,:|,|:,:/,/:,:\,\:' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
@@ -11,7 +11,13 @@ end
 end
 return {
 patterns = {
-    "😐"
+    "😐",
+    ":|",
+    "|:",
+    ":/",
+    :/:",
+    "\:",
+    ":\",
 },
 run = run
 }
