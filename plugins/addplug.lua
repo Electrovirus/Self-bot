774f3d48b976e95d1f97142f292190bf519160c2
Seyed Ali Kia <seyedkia27@gmail.com>
2016-06-24 15:43:36 +0430

    Update addplug.lua

--- plugins/addplug.lua
+++ plugins/addplug.lua
@@ -11,14 +11,14 @@ local function run(msg, matches)
   file:write(text)
   file:flush()
   file:close()
-  return "Done ;-)" 
+  return ">پلاگین ذخیره شد !" 
 end
 end 
 return {
   description = "a Usefull plugin for sudo !",  
   usage = "A plugins to add Another plugins to the server",
   patterns = {
-    "^#addplug +(.+) (.*)$"
+    "^[Aa]ddplug +(.+) (.*)$"
   },
   run = run
 }
