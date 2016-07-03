51f50112ee36151229a9d26c5f7eb6602baa6365
Electrovirus <E.electrovirus@gmail.com>
2016-07-03 22:35:28 +0430

    Update ctrl.lua

--- plugins/ctrl.lua
+++ plugins/ctrl.lua
@@ -26,13 +26,13 @@ local function enable_channel(receiver, to_id)
 	end
 
 	if _config.disabled_channels[receiver] == nil then
-		return 'Selfbot is on🔔'
+		return 'Selfbot is online▶️'
 	end
 	
 	_config.disabled_channels[receiver] = false
 
 	save_config()
-	return 'Selfbot is off🔕'
+	return 'Selfbot is offline⏹'
 end
 
 local function disable_channel(receiver, to_id)
@@ -43,7 +43,7 @@ local function disable_channel(receiver, to_id)
 	_config.disabled_channels[receiver] = true
 
 	save_config()
-	return 'Selfbot is off🔕'
+	return 'Selfbot is offline⏹'
 end
 
 local function pre_process(msg)
